d6e8b2d99f0ea42a5af20f954ac4afbbdb11562f
Manvi Dewangan <109518835+Manvi1902@users.noreply.github.com>
2023-07-13 14:03:21 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './CSSFile/index.css';
+import App from './App';
+/*import Context from './Context/Context;
+<Context>
+     <App />
+ </Context>
+*/
+
+ReactDOM.render(
+ <>
+ 
+     <App />
+ 
+ </>
+
+  , document.getElementById('root')
+);
+
---
+++ src/productData.js
@@ -0,0 +1,52 @@
+ const  productData=[
+  {
+    "id":0,
+    "img":"https://burst.shopifycdn.com/photos/cozy-livingroom-with-window.jpg?&format=pjpg&exif=1&iptc=1",
+    "title": "furniture",
+    "category":"loremispusumm",
+    "price": 300,
+    "quantity":1
+  },
+  {
+      "id":1,
+      "img": "../images/img1.jpg",
+      "title": "furniture",
+      "category":"loremispusumm",
+      "price": 300,
+      "quantity":1
+  },
+  {
+    "id":2,
+    "img": "https://burst.shopifycdn.com/photos/cozy-livingroom-with-window.jpg?&format=pjpg&exif=1&iptc=1",
+    "title": "furniture",
+    "category":"loremispusumm",
+    "price": 300,
+    "quantity":1
+  },
+  {
+    "id":3,
+    "img": "https://burst.shopifycdn.com/photos/grey-couch-with-a-bookcase-behind-it.jpg?&format=pjpg&exif=1&iptc=1",
+    "title": "furniture",
+    "category":"loremispusumm",
+    "price": 300,
+    "quantity":1
+  },
+  {
+    "id":4,
+    "img": "https://burst.shopifycdn.com/photos/perfect-morning-patio.jpg?&format=pjpg&exif=1&iptc=1",
+    "title": "furniture",
+    "category":"loremispusumm",
+    "price": 300,
+    "quantity":1
+  },
+  {
+    "id":5,
+    "img": "https://burst.shopifycdn.com/photos/living-room-designed-in-grey-and-gold.jpg?width=373&format=pjpg&exif=1&iptc=1",
+    "title": "furniture",
+    "category":"loremispusumm",
+    "price": 300,
+    "quantity":1
+  }
+
+]
+export default productData;
